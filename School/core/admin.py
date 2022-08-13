93c8d55433c635cd0bb1cf33ff6e74cff1673836
xunruan <xunruan@icloud.com>
2022-08-13 12:22:08 -0400

    Override AdminForm Add validation

--- School/core/admin.py
+++ School/core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django import forms
 from django.urls import reverse
 from django.utils.html import format_html
 from django.utils.http import urlencode
@@ -6,19 +7,32 @@ from django.utils.http import urlencode
 from core.models import Person, Course, Grade
 
 
+class PersonAdminForm(forms.ModelForm):
+    class Meta:
+        model = Person
+        fields = "__all__"
+
+    def clean_first_name(self):
+        if self.cleaned_data["first_name"] == "Spike":
+            raise forms.ValidationError("No Vampires")
+
+        return self.cleaned_data["first_name"]
+
+
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
     list_display = ("last_name", "first_name", "show_average")
-    search_fields = ("last_name__startswith", )  # Tuple
+    search_fields = ("last_name__startswith",)  # Tuple
     fields = ("first_name", "last_name", "courses")
+    form = PersonAdminForm
 
     def show_average(self, obj):  # for all people
         from django.db.models import Avg
         from django.utils.html import format_html
 
-        grades = Grade.objects.filter(person=obj)   # get all grades of the current person
-        print(type(grades))     # query set
-        result = grades.aggregate(Avg("grade"))     # get aa dictionary of the aggregate
+        grades = Grade.objects.filter(person=obj)  # get all grades of the current person
+        print(type(grades))  # query set
+        result = grades.aggregate(Avg("grade"))  # get aa dictionary of the aggregate
         return format_html("<b><i>{}</i></b>", result["grade__avg"])
 
     show_average.short_description = "Average"
@@ -35,14 +49,14 @@ class PersonAdmin(admin.ModelAdmin):
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ("name", "year", "view_students_link")
-    list_filter = ("year", )
+    list_filter = ("year",)
 
     def view_students_link(self, obj):
         count = obj.person_set.count()
         url = (
-            reverse("admin:core_person_changelist")  # admin page name convention: admin:%(app)s_%(model)s_%(page)
-            + "?"
-            + urlencode({"courses__id": f"{obj.id}"})
+                reverse("admin:core_person_changelist")  # admin page name convention: admin:%(app)s_%(model)s_%(page)
+                + "?"
+                + urlencode({"courses__id": f"{obj.id}"})
         )
         return format_html('<a href="{}">{} Students</a>', url, count)
 
@@ -52,4 +66,3 @@ class CourseAdmin(admin.ModelAdmin):
 @admin.register(Grade)
 class GradeAdmin(admin.ModelAdmin):
     pass
-
